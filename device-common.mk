9e507b0d347b7ac045100c329a336ed2d47cb61e
ma3axata <v.radulevich@gmail.com>
2021-02-25 22:54:23 +0200

    e5-common: fix compilation

--- device-common.mk
+++ device-common.mk
@@ -30,11 +30,11 @@ TARGET_SCREEN_DENSITY := 320
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+	$(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Platform info config
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
+	$(DEVICE_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += \
